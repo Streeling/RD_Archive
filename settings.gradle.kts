189d3425e8b84e376b8c96190b53aa858897c0ff
Radu Dumbraveanu <rdumbraveanu@amsoft-group.com>
2024-05-20 22:02:48 +0300

    Rename module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,15 @@
 rootProject.name = "RD_Archive"
 include("bjug:pros-non-functional-java")
 findProject(":bjug:pros-non-functional-java")?.name = "pros-non-functional-java"
-include("amsoft")
-include("amsoft:oauth2-resource-server")
-findProject(":amsoft:oauth2-resource-server")?.name = "oauth2-resource-server"
-include("amsoft:oauth2-login")
-findProject(":amsoft:oauth2-login")?.name = "oauth2-login"
-include("amsoft:oauth2-client")
-findProject(":amsoft:oauth2-client")?.name = "oauth2-client"
-include("amsoft:gpt2-in-about-150-lines-of-nd4j")
-findProject(":amsoft:gpt2-in-about-150-lines-of-nd4j")?.name = "gpt2-in-about-150-lines-of-nd4j"
+include("ams")
+include("ams:oauth2-resource-server")
+findProject(":ams:oauth2-resource-server")?.name = "oauth2-resource-server"
+include("ams:oauth2-login")
+findProject(":ams:oauth2-login")?.name = "oauth2-login"
+include("ams:oauth2-client")
+findProject(":ams:oauth2-client")?.name = "oauth2-client"
+include("ams:gpt2-in-about-150-lines-of-nd4j")
+findProject(":ams:gpt2-in-about-150-lines-of-nd4j")?.name = "gpt2-in-about-150-lines-of-nd4j"
 include("itlab")
 include("itlab:strings")
 findProject(":itlab:strings")?.name = "strings"
